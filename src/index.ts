71dd6fb3695888d7dad98fdaac14a63614156ca3
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-07 12:17:11 -0400

    apollo server express config, add get routes for all posts, post by id

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -9,11 +14,25 @@ const main = async () => {
   // run migration
   await orm.getMigrator().up();
 
-  // create Post object
-  const post = await orm.em.create(Post, { title: "my first post" });
+  const app = express();
 
-  // insert
-  await orm.em.persistAndFlush(post);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server listening on port 4000");
+  });
+
+  app.get("/", (_, res) => {
+    res.status(200).send("hello world");
+  });
 };
 
 main();
---
+++ src/resolvers/post.ts
@@ -0,0 +1,19 @@
+import { Resolver, Query, Ctx, Int, Arg } from "type-graphql";
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+}
