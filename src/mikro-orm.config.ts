1008be41224e0aa8bfdd731c1af2ed72a9ce4a85
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-07 14:03:36 -0400

    add users table + login/reg mutations

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,5 @@
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
@@ -6,7 +7,7 @@ import path from "path";
 require("dotenv").config();
 
 export default {
-  entities: [Post],
+  entities: [Post, User],
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
